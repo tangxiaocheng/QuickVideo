01662bfa4ff7e31c697c1da218f9c7a053cbaf19
Randy <tangdengcheng@gmail.com>
2020-10-20 11:46:34 -0700

    add gson converter

--- app/src/androidTest/java/com/datacubed/video/recorder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/datacubed/video/recorder/ExampleInstrumentedTest.java
@@ -3,10 +3,18 @@ package com.datacubed.video.recorder;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
+import android.util.Log;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import com.datacubed.video.recorder.network.GitHubService;
+import com.datacubed.video.recorder.network.Repo;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,5 +29,24 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
     assertEquals("com.datacubed.video.recorder", appContext.getPackageName());
+  }
+  @Test
+  public void retrofit(){
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .build();
+    GitHubService service = retrofit.create(GitHubService.class);
+    Call<List<Repo>> call = service.listRepos("octocat");
+    call.enqueue(new Callback<List<Repo>>() {
+      @Override
+      public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+        Log.i("TAG", response.toString());
+      }
+
+      @Override
+      public void onFailure(Call<List<Repo>> call, Throwable t) {
+
+      }
+    });
   }
 }
--- app/src/main/java/com/datacubed/video/recorder/network/NetworkActivity.java
+++ app/src/main/java/com/datacubed/video/recorder/network/NetworkActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.datacubed.video.recorder.R;
 import java.util.List;
+import retrofit.GsonConverterFactory;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -13,6 +14,7 @@ public class NetworkActivity extends AppCompatActivity {
 
   Retrofit retrofit = new Retrofit.Builder()
       .baseUrl("https://api.github.com/")
+//      .addConverterFactory(GsonConverterFactory.create())
       .build();
 
   @Override
@@ -20,19 +22,7 @@ public class NetworkActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_network);
 
-    GitHubService service = retrofit.create(GitHubService.class);
-    Call<List<Repo>> call = service.listRepos("octocat");
-    call.enqueue(new Callback<List<Repo>>() {
-      @Override
-      public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
 
-      }
-
-      @Override
-      public void onFailure(Call<List<Repo>> call, Throwable t) {
-
-      }
-    });
 
   }
 }
