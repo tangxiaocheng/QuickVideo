20abfcb41d8cb20cd53795190afee0f77cf851fe
Randy <tangdengcheng@gmail.com>
2020-10-20 16:31:49 -0700

    Update ViewPager to ViewPager2 and review code.

--- app/src/main/java/com/datacubed/video/recorder/feature/main/BottomNavItemSelectedListener.java
+++ app/src/main/java/com/datacubed/video/recorder/feature/main/BottomNavItemSelectedListener.java
@@ -1,19 +1,20 @@
 package com.datacubed.video.recorder.feature.main;
 
+import static com.datacubed.video.recorder.feature.main.MainAdapter.IDS_MENU;
+
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.viewpager.widget.ViewPager;
-import com.datacubed.video.recorder.R;
+import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNavItemSelectedListener
     implements BottomNavigationView.OnNavigationItemSelectedListener {
 
   private final ActionBar actionBar;
-  private final ViewPager viewPager;
+  private final ViewPager2 viewPager;
 
-  public BottomNavItemSelectedListener(ViewPager viewPager, ActionBar actionBar) {
+  public BottomNavItemSelectedListener(ViewPager2 viewPager, ActionBar actionBar) {
     this.actionBar = actionBar;
     this.viewPager = viewPager;
   }
@@ -22,12 +23,11 @@ public class BottomNavItemSelectedListener
   public boolean onNavigationItemSelected(@NonNull MenuItem item) {
     actionBar.setTitle(item.getTitle());
     int itemId = item.getItemId();
-    if (itemId == R.id.navigation_record_fragment) {
-      viewPager.setCurrentItem(0);
-      return true;
-    } else if (itemId == R.id.navigation_saved_recordings_fragment) {
-      viewPager.setCurrentItem(1);
-      return true;
+    for (int i = 0; i < IDS_MENU.length; i++) {
+      if (itemId == IDS_MENU[i]) {
+        viewPager.setCurrentItem(i);
+        return true;
+      }
     }
     return false;
   }
--- app/src/main/java/com/datacubed/video/recorder/feature/main/MainActivity.java
+++ app/src/main/java/com/datacubed/video/recorder/feature/main/MainActivity.java
@@ -1,45 +1,52 @@
 package com.datacubed.video.recorder.feature.main;
 
+import static com.datacubed.video.recorder.feature.main.MainAdapter.IDS_MENU;
+
 import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager.widget.ViewPager.OnPageChangeListener;
+import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback;
 import com.datacubed.video.recorder.R;
 import com.datacubed.video.recorder.feature.home.HomeFragment.Callback;
-import com.datacubed.video.recorder.util.Constant;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements OnPageChangeListener, Callback {
+public class MainActivity extends AppCompatActivity implements Callback {
 
   private BottomNavigationView navigation;
+  OnPageChangeCallback onPageChangeCallback = new OnPageChangeCallback() {
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+      super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+      super.onPageSelected(position);
+      navigation.setSelectedItemId(IDS_MENU[position]);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+      super.onPageScrollStateChanged(state);
+    }
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     navigation = findViewById(R.id.nav_view);
-    ViewPager viewPager = findViewById(R.id.host_view_pager);
-    viewPager.addOnPageChangeListener(this);
-    viewPager.setAdapter(new MainAdapter(getSupportFragmentManager(), this));
+    ViewPager2 viewPager = findViewById(R.id.host_view_pager);
+    viewPager.setAdapter(new MainAdapter(MainActivity.this, this));
+    viewPager.registerOnPageChangeCallback(onPageChangeCallback);
     ActionBar actionBar = getSupportActionBar();
     navigation.setOnNavigationItemSelectedListener(
         new BottomNavItemSelectedListener(viewPager, actionBar));
   }
 
   @Override
-  public void onPageSelected(int position) {
-    navigation.setSelectedItemId(MainAdapter.mapToMenuId(position));
-  }
-
-  @Override
-  public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
-  @Override
-  public void onPageScrollStateChanged(int state) {}
-
-  @Override
-  public void onNewVideoRecorded() {
-    navigation.setSelectedItemId(MainAdapter.mapToMenuId(Constant.VIDEO_LIST));
+  public void onNewVideoRecorded(int pos) {
+    navigation.setSelectedItemId(IDS_MENU[pos]);
   }
 }
