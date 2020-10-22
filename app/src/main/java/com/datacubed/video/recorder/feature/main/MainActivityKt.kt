934a8e38cdb56e422aafbd9e1f4a49bd28cb70ab
Randy <tangdengcheng@gmail.com>
2020-10-22 16:50:05 -0700

    Step 1: Initialize Kotlin development Added Kotlin dependencies

---
+++ app/src/main/java/com/datacubed/video/recorder/feature/main/MainActivityKt.kt
@@ -0,0 +1,12 @@
+package com.datacubed.video.recorder.feature.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.datacubed.video.recorder.R
+
+class MainActivityKt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kt)
+    }
+}
