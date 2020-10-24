3f7987300be34fafa5f03cf95ad36beb288613a6
Randy <tangdengcheng@gmail.com>
2020-10-24 08:23:59 -0700

    Add Entry for Both Java and Kotlin

---
+++ app/src/main/java/com/datacubed/video/recorder/feature/main/EntryActivity.kt
@@ -0,0 +1,22 @@
+package com.datacubed.video.recorder.feature.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.datacubed.video.recorder.R
+
+class EntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry_acitviy)
+        findViewById<View>(R.id.kotlin_button).setOnClickListener {
+            val intent = Intent(this, MainActivityKt::class.java)
+            startActivity(intent)
+        }
+        findViewById<View>(R.id.java_button).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/datacubed/video/recorder/feature/main/MainActivityKt.kt
+++ app/src/main/java/com/datacubed/video/recorder/feature/main/MainActivityKt.kt
@@ -2,11 +2,21 @@ package com.datacubed.video.recorder.feature.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.datacubed.video.recorder.R
+import com.datacubed.video.recorder.feature.home.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivityKt : AppCompatActivity() {
+class MainActivityKt : AppCompatActivity(), HomeFragment.Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_kt)
+        setContentView(R.layout.activity_main)
+        var navigation: BottomNavigationView = findViewById(R.id.nav_view)
+        val viewPager: ViewPager2 = findViewById(R.id.host_view_pager)
+//        viewPager.adapter(MainAdapter(this,this))
+    }
+
+    override fun onNewVideoRecorded(pos: Int) {
+        TODO("Not yet implemented")
     }
 }
